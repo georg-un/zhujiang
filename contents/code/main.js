9689561696f58c165aa53759d38be3100af84503
Georg Unterholzner <georg.unterholzner.coding@gmail.com>
2022-11-27 19:59:27 +0100

    Clean up

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,11 +1,5 @@
 var Zhujiang = {};
 
-/**
- * Margin of Error in PX for centering a client. If client's center is within
- * this PX of workspace center, consider the client centered.
- */
-Zhujiang.CENTER_MOE = 2;
-
 Zhujiang.MARGIN_OF_ERROR = 1;
 
 Zhujiang.States = {
@@ -71,14 +65,6 @@ Zhujiang.getWorkAreaRect = function () {
 };
 
 /**
- * @param {QRect} rect
- * @return {number}
- */
-Zhujiang.getRightEdge = function (rect) {
-  return rect.x + rect.width;
-};
-
-/**
  * @param {number} begin e.g. 33.33
  * @param {number} end e.g. 1
  * @param {number} workAreaMinX e.g. 0
@@ -92,20 +78,6 @@ Zhujiang.sizeToLeftAndRightEdge = function (begin, end, workAreaMinX, workAreaMa
 		Math.round((begin / 100) * workAreaWidth + workAreaMinX),
 		Math.round((end / 100) * workAreaWidth + workAreaMinX)
 	];
-}
-
-Zhujiang.buildCenteredWidth = function (size, workAreaMinX, workAreaMaxX, workAreaWidth) {
-    return Math.round(Math.round((begin / 100) * workAreaWidth + workAreaMinX))
-}
-
-Zhujiang.sortSizes = function(a, b) {
-    if (a[0] > b[0]) return 1;
-    if (a[0] < b[0]) return -1;
-    if (a[0] === b[0]) {
-        if (a[1] > b[1]) return 1;
-        if (a[1] < b[1]) return -1;
-        return 0;
-    }
 }
 
 Zhujiang.buildSizes = function () {
@@ -267,103 +239,10 @@ Zhujiang.setNextSize = function (client, shouldDecrease, centered) {
 		return Zhujiang.States.ERROR;
 	}
 
-	/*var nextSize = Zhujiang.isFirstMove(client, centered) ?
-	    Zhujiang.getNextSizeForFirstMove(shouldDecrease, centered) :
-    	Zhujiang.getNextSize(client, shouldDecrease, centered);*/
-
     return Zhujiang.isFirstMove(client, centered) ?
         Zhujiang.resizeWindowFirstMove(client, shouldDecrease, centered) :
         Zhujiang.resizeWindow(client, shouldDecrease, centered);
-
-    /*var rect = client.geometry;
-    rect.x = nextSize[0];
-    rect.width = nextSize[1] - nextSize[0];
-    rect.right = rect.x + rect.width;
-    return Zhujiang.States.DONE;*/
 }
-
-
-/**
- * @param {number} size e.g. 33.3333
- * @return {number} width e.g. 359.9
- */
-Zhujiang.sizeToWidth = function (size) {
-  return size / 100 * Zhujiang.getWorkAreaRect().width;
-};
-
-/**
- * @param {number} clientWidth of client e.g. 359.9
- * @return {number} index in Sizes array. The size is the nearest size to the
- * client width in relation to the workspace width.
- */
-Zhujiang.widthToSizeIndex = function (clientWidth) {
-  // E.g. if window is 650px and screen is 1080 we'd get 33
-  var intWidthPercent = Math.round(clientWidth / Zhujiang.getWorkAreaRect().width * 100);
-
-  var smallestDiff = 9999;
-  var smallestI = 0;
-  Zhujiang.Sizes.forEach(function (size, i) {
-    var diff = Math.abs(intWidthPercent - size);
-    if (diff < smallestDiff) {
-      smallestDiff = diff;
-      smallestI = i;
-    }
-  });
-  return smallestI;
-};
-
-/**
- * @param {number} i index in Sizes
- * @return {number} valid index in Sizes
- */
-Zhujiang.getNextI = function (i) {
-  return i >= Zhujiang.Sizes.length - 1 ? 0 : i + 1;
-};
-
-/**
- * @param {number} clientWidth like 500 (px)
- * @return {number} width like 333 (px)
- */
-Zhujiang.getNextWidth = function (clientWidth) {
-  // e.g. 2 is 50, meaning the client is roughly 50% of the workspace width
-  var sizeI = Zhujiang.widthToSizeIndex(clientWidth);
-  var nextI = Zhujiang.getNextI(sizeI); // would go left 1 or center/right to 3
-  var nextSize = Zhujiang.Sizes[nextI]; // 33.3333 or 66.6666
-  var nextWidth = Zhujiang.sizeToWidth(nextSize); // whatever px value, e.g. 359.9
-  return parseInt(nextWidth, 10);
-};
-
-Zhujiang.AfterCycle = {};
-Zhujiang.AfterCycle[Zhujiang.Dirs.Left] = function afterCycleLeft(client) {
-  return Zhujiang.States.DONE;
-};
-Zhujiang.AfterCycle[Zhujiang.Dirs.Up] = function afterCycleCenter(client) {
-  return Zhujiang.Move[Zhujiang.Dirs.Up](client);
-};
-Zhujiang.AfterCycle[Zhujiang.Dirs.Right] = function afterCycleRight(client) {
-  return Zhujiang.Move[Zhujiang.Dirs.Right](client);
-};
-
-/**
- * @param {KWin::AbstractClient} client
- * @param {string} dir 'Left'
- * @return {string} Zhujiang.States value
- */
-Zhujiang.cycle = function (client, dir) {
-  if (!client.resizeable) {
-    return Zhujiang.States.ERROR;
-  }
-
-  var rect = client.geometry; // { width, height, x, y }
-  var clientWidth = rect.width; // 500
-  var nextWidth = Zhujiang.getNextWidth(clientWidth);
-  rect.width = nextWidth;
-  client.geometry = rect;
-
-  // Move again after cycle to fix reposition due to resize
-  var after = Zhujiang.AfterCycle[dir];
-  return after && after(client);
-};
 
 /**
  * Unmaximize a window without changing size.
@@ -378,7 +257,6 @@ Zhujiang.unmax = function (client) {
     var HORIZONTAL = false;
     var maxedRect = client.geometry;
     client.setMaximize(VERTICAL, HORIZONTAL);
-
     // Restore previous maximized size, but now unmaxed so has drop shadows
     // and window borders.
     client.geometry = maxedRect;
@@ -395,111 +273,11 @@ Zhujiang.beforeMove = function (client) {
   if (!client.moveable) {
     return Zhujiang.States.ERROR;
   }
-
   // Horizonatally unmax a client before moving, since you shouldn't be able
   // move a maximized window.
   // setMaximize is documented at https://develop.kde.org/docs/plasma/kwin/api/
   Zhujiang.unmax(client);
   return Zhujiang.States.DONE;
-};
-
-Zhujiang.Move = {};
-
-/**
- * @param {KWin::AbstractClient} client
- * @return {string} Zhujiang.States value
- */
-Zhujiang.Move[Zhujiang.Dirs.Left] = function (client) {
-  if (Zhujiang.beforeMove(client) === Zhujiang.States.ERROR) {
-    return Zhujiang.States.ERROR;
-  }
-
-  var rect = client.geometry;
-  var workAreaLeftEdge = Zhujiang.getWorkAreaRect().x;
-  var isFlushed = rect.x === workAreaLeftEdge;
-  if (isFlushed) {
-    return Zhujiang.States.NOOP;
-  }
-
-  var rect = client.geometry;
-  rect.x = workAreaLeftEdge;
-  client.geometry = rect;
-  return Zhujiang.States.DONE;
-};
-
-/**
- * @param {KWin::AbstractClient} client
- * @return {string} Zhujiang.States value
- */
-Zhujiang.Move[Zhujiang.Dirs.Right] = function (client) {
-  if (Zhujiang.beforeMove(client) === Zhujiang.States.ERROR) {
-    return Zhujiang.States.ERROR;
-  }
-
-  var rect = client.geometry;
-  var clientRightEdge = Zhujiang.getRightEdge(rect);
-
-  var workAreaRect = Zhujiang.getWorkAreaRect();
-  var workAreaRightEdge = Zhujiang.getRightEdge(workAreaRect);
-
-  var isFlushed = clientRightEdge === workAreaRightEdge;
-  if (isFlushed) {
-    return Zhujiang.States.NOOP;
-  }
-
-  rect.x = workAreaRightEdge - rect.width;
-  client.geometry = rect;
-  return Zhujiang.States.DONE;
-};
-
-/**
- * @param {KWin::AbstractClient} client
- * @return {string} Zhujiang.States value
- */
-Zhujiang.Move[Zhujiang.Dirs.Up] = function (client) {
-  if (Zhujiang.beforeMove(client) === Zhujiang.States.ERROR) {
-    return Zhujiang.States.ERROR;
-  }
-
-  var rect = client.geometry;
-  var clientWidth = rect.width;
-  var workAreaRect = Zhujiang.getWorkAreaRect();
-  var workspaceCenterX = (workAreaRect.width / 2) + workAreaRect.x;
-  var clientCenterX = rect.x + (clientWidth / 2);
-  var isCentered = (
-    clientCenterX - Zhujiang.CENTER_MOE <= workspaceCenterX &&
-    clientCenterX + Zhujiang.CENTER_MOE >= workspaceCenterX
-  );
-  if (isCentered) {
-    return Zhujiang.States.NOOP;
-  }
-
-  var distance = workspaceCenterX - clientCenterX;
-  rect.x = rect.x + distance;
-  client.geometry = rect;
-  return Zhujiang.States.DONE;
-};
-
-/**
- * @param {KWin::AbstractClient} client
- * @param {string} key
- * @return {string} Zhujiang.States value
- */
-Zhujiang.squish = function (client, key) {
-  var dir = Zhujiang.Dirs[key];
-  var move = Zhujiang.Move[key];
-  if (!move || !dir) {
-    print('Unrecognized command');
-    return Zhujiang.States.ERROR;
-  }
-
-  var result = move(client);
-  if (result === Zhujiang.States.NOOP || result === Zhujiang.States.DONE) {
-    return Zhujiang.cycle(client, dir);
-  }
-
-  print('Failed to move ' + dir);
-  return Zhujiang.States.ERROR;
 };
 
 /**
